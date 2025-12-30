68630ab301e8aa3e39786d52e2da0a633b900733
Radhy Shyam <radhy2929@gmail.com>
2025-12-30 23:20:11 +0530

    end of the day

--- script.js
+++ script.js
@@ -1,2 +1,42 @@
 // object practice
 
+// let obj ={
+//     a : 3,
+//     b : 4, 
+//     c : 5,
+//     $ : 5,
+//     25 : 44,
+//     "25" : 55,
+//     true : 555
+// }
+
+// console.log(obj['true'])
+
+
+// let obj = {
+//     [2+5] : "a",
+//     8 : "b"
+// }
+
+// console.log(Object.keys(obj)[0]);
+
+
+console.log(typeof(null));
+
+
+// for(let radhey in obj ){
+//     console.log(` this is the key ${radhey} and this is the value ${obj[radhey]}`)
+// // }
+// let obj = {}
+
+// Object.defineProperties(obj,{
+//    name : {
+//     value : 'radhey',
+//     writable : false,
+//     enumerable : true,
+//     configurable : false
+//    }
+// })
+// // delete obj.name
+// obj.name = 'payaa'
+// console.log(obj);
